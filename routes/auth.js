4280dfa0e548865d5fe1bf094026638007fb5624
Bairon Developer <baironf.bernalg@ecci.edu.co>
2023-07-24 12:17:04 -0500

    renew jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, renewToken } = require('../controllers/auth');
 const { validateJWT } = require('../middlewares/auth/validate-jwt');
-const { getUsers } = require('../controllers/user');
 
 const router = Router();
 
@@ -11,5 +10,11 @@ router.post('/', [
     check('password', 'el password es obligatorio').not().isEmpty(),
     check('email', 'el email es obligatorio').isEmail(),
 ], login)
+
+
+router.get('/renew',
+    validateJWT,
+    renewToken
+)
 
 module.exports = router;
